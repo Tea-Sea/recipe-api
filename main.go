f0c6d0ca8a218032d8997357745a9241101a2b24
Tea-Sea <thomcaspar@gmail.com>
2025-08-19 09:43:28 +0100

    set up env variable

--- main.go
+++ main.go
@@ -18,7 +18,13 @@ import (
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO: Edit CORs settings in future
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		frontend, ok := os.LookupEnv("FRONTEND_URL")
+		if !ok {
+			fmt.Println("FRONTEND_URL is not set")
+		} else {
+			fmt.Printf("FRONTEND_URL = %s\n", frontend)
+		}
+		w.Header().Set("Access-Control-Allow-Origin", frontend)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
